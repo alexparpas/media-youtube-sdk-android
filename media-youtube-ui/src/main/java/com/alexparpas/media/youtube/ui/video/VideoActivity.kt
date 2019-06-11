f21dbabe2f4bc811c792298851337d38c9e02a4b
Alex Parpas <alex.parpas@incrowdsports.com>
2019-06-11 20:37:07 +0100

    - Improved video player to handle full screen changes better. - Add video card background style option

--- media-youtube-ui/src/main/java/com/alexparpas/media/youtube/ui/video/VideoActivity.kt
+++ media-youtube-ui/src/main/java/com/alexparpas/media/youtube/ui/video/VideoActivity.kt
@@ -1,50 +1,106 @@
 package com.alexparpas.media.youtube.ui.video
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.alexparpas.media.youtube.ui.MediaYouTubeUi
 import com.alexparpas.media.youtube.R
-import com.pierfrancescosoffritti.androidyoutubeplayer.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.player.listeners.YouTubePlayerFullScreenListener
+import com.alexparpas.media.youtube.ui.MediaYouTubeUi
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerFullScreenListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.loadOrCueVideo
 import kotlinx.android.synthetic.main.myt_activity_video.*
 
+
 internal class VideoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.myt_activity_video)
 
         val videoId = intent.getStringExtra(MediaYouTubeUi.ARG_VIDEO_ID)
+        initPlayer(videoId)
+    }
+
+    override fun onConfigurationChanged(newConfiguration: Configuration) {
+        super.onConfigurationChanged(newConfiguration)
+        youtube_player_view.getPlayerUiController().getMenu()?.dismiss()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    override fun onBackPressed() {
+        if (youtube_player_view.isFullScreen()) {
+            exitFullScreen()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun initPlayer(videoId: String) {
+        // The player will automatically release itself when the activity is destroyed.
+        // The player will automatically pause when the activity is stopped
         lifecycle.addObserver(youtube_player_view)
 
-        youtube_player_view.initialize({ youTubePlayer ->
-            youTubePlayer.addListener(object : AbstractYouTubePlayerListener() {
-                override fun onReady() {
-                    super.onReady()
-                    youTubePlayer.loadVideo(videoId, 0f)
-                }
-            })
-        }, true)
+        youtube_player_view.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                super.onReady(youTubePlayer)
 
+                youTubePlayer.loadOrCueVideo(
+                        lifecycle = lifecycle,
+                        videoId = videoId,
+                        startSeconds = 0f
+                )
+            }
+        })
+        addFullScreenListenerToPlayer()
+    }
+
+    private fun addFullScreenListenerToPlayer() {
         youtube_player_view.addFullScreenListener(object : YouTubePlayerFullScreenListener {
             override fun onYouTubePlayerEnterFullScreen() {
-                hideSystemUI(window.decorView)
+                enterFullScreen()
             }
 
             override fun onYouTubePlayerExitFullScreen() {
-                //no-op
+                exitFullScreen()
             }
         })
-
-        youtube_player_view.enterFullScreen()
     }
 
-    private fun hideSystemUI(decorView: View) {
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    private fun enterFullScreen() {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        hideSystemUI()
+    }
+
+    private fun exitFullScreen() {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        showSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    // Shows the system bars by removing all the flags
+    // except for the ones that make the content appear under the system bars.
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 }
