c67bef7733f5897b552fc6c3445f10577bc3bc10
Alex Parpas <alex.parpas@incrowdsports.com>
2019-06-14 18:12:15 +0100

    Now hiding system ui when video is on full screen.

--- media-youtube-ui/src/main/java/com/alexparpas/media/youtube/ui/video/VideoActivity.kt
+++ media-youtube-ui/src/main/java/com/alexparpas/media/youtube/ui/video/VideoActivity.kt
@@ -2,6 +2,7 @@ package com.alexparpas.media.youtube.ui.video
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.alexparpas.media.youtube.R
 import com.alexparpas.media.youtube.ui.MediaYouTubeUi
@@ -63,11 +64,34 @@ internal class VideoActivity : AppCompatActivity() {
 
     private fun enterFullScreen() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        hideSystemUI()
         youtube_player_view.enterFullScreen()
     }
 
     private fun exitFullScreen() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        showSystemUI()
         youtube_player_view.exitFullScreen()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    // Shows the system bars by removing all the flags
+    // except for the ones that make the content appear under the system bars.
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 }
